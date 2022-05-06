065d5a70b71ccefcaaed8d8877d5f077b3295e8d
Михаил <mishak2005med@mail.ru>
2022-05-06 22:34:11 +0400

    feat: add some unworked views
    Да, пока они не очень работают

--- tutorhunt/settings.py
+++ tutorhunt/settings.py
@@ -106,6 +106,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

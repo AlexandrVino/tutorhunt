bbf9b0b7dd36568c77f499ffe63f62465993c86a
Михаил <mishak2005med@mail.ru>
2022-05-13 20:26:55 +0400

    feat: develop model

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from notifications.models import NotificationModel
 
-# Register your models here.
+
+@admin.register(NotificationModel)
+class NotificationAdmin(admin.ModelAdmin):
+    empty_value_display = "(null)"
+    list_display = ("__str__", "category", "recipient", "initiator", "creation")
--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class NotificationModel(models.Model):
+    category = models.CharField("категория", max_length=20)
+    message = models.TextField("сообщение")
+    read = models.BooleanField("прочитано", default=False)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_notifications")
+    initiator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="caused_notifications",
+                                  blank=True, null=True, default=None)
+    creation = models.DateTimeField("дата получения", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "уведомление"
+        verbose_name_plural = "уведомления"

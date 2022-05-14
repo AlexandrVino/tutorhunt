c499b101efc0cddf1a88849cafd29be0b5eaf169
Михаил <mishak2005med@mail.ru>
2022-05-14 17:31:33 +0400

    feat: add editable read field

--- notifications/admin.py
+++ notifications/admin.py
@@ -5,4 +5,5 @@ from notifications.models import NotificationModel
 @admin.register(NotificationModel)
 class NotificationAdmin(admin.ModelAdmin):
     empty_value_display = "(null)"
-    list_display = ("__str__", "category", "recipient", "initiator", "creation")
+    list_display = ("category", "recipient", "initiator", "creation", "read")
+    list_editable = ("read", )

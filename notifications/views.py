c5ed249ea069dfafe3957ff587c98aa7effd47cb
Михаил <mishak2005med@mail.ru>
2022-05-16 16:23:10 +0400

    feat: some simple fixes

--- notifications/views.py
+++ notifications/views.py
@@ -1,4 +1,5 @@
-from typing import Dict, Type
+from ctypes import Union
+from typing import Any, Dict, Type
 from unicodedata import category
 from django.forms import ModelForm
 from django.http import HttpRequest, HttpResponse
@@ -18,14 +19,10 @@ class NotificationListView(TemplateView):
     template_name = "notifications/view_notifications.html"
     paginate_by = 5
     queryset = NotificationModel.objects.all()
-    form_initial = None
+    form_initial: Dict[str, Any] = {}
     
     def dispatch(self, request: HttpRequest, *args, **kwargs):
-        if "page" in self.kwargs and self.kwargs["page"] > self.get_paginator().num_pages:
-            return redirect(reverse("notifications"))
-
         self.kwargs.setdefault("page", 1)
-
         return super().dispatch(request, *args, **kwargs)
 
     def get_form_class(self) -> Type[ModelForm]:
@@ -35,7 +32,7 @@ class NotificationListView(TemplateView):
     
     def get_queryset(self) -> NotificationQueryset:
         if self.request.user.is_staff:
-            return self.queryset.order_by("id")
+            return self.queryset.order_by("-creation")
         return (
             self.queryset
                 .by_recipient(self.request.user)
@@ -46,12 +43,20 @@ class NotificationListView(TemplateView):
         return Paginator(self.get_queryset(), per_page=self.paginate_by, orphans=3)
 
     def get_form(self):
-        return self.get_form_class()(initial=self.form_initial)
+        return self.get_form_class()(initial=self.get_form_initial())
+
+    def get_form_initial(self) -> Dict[str, Any]:
+        initial = self.form_initial
+
+        if self.request.user.is_staff:
+            initial.setdefault("recipient", self.request.user)
+
+        return initial
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        context["form"] = self.get_form_class()(initial=self.form_initial)
+        context["form"] = self.get_form()
 
         paginator = self.get_paginator()
         context["objects"] = paginator.page(self.kwargs["page"])
@@ -61,12 +66,17 @@ class NotificationListView(TemplateView):
     
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         self.get_queryset().by_recipient(request.user).mark_read()
+
+        if self.kwargs["page"] > self.get_paginator().num_pages:
+            return redirect(reverse("notifications"))
+
         return super().get(request, *args, **kwargs)
     
     def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        self.form_initial = request.POST
+        self.form_initial = self.request.POST.dict()
         
-        category, recipient = request.POST.get("category"), request.POST.get("recipient")
+        category, recipient = (self.request.POST.get("category"),
+                               self.request.POST.get("recipient"))
 
         if recipient:
             self.queryset = self.queryset.by_recipient(recipient)

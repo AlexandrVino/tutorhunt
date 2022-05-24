bf0f5c66576b7a50a3744cadc448584608d301e0
Михаил <mishak2005med@mail.ru>
2022-05-24 15:13:00 +0400

    fix: edit text according to russian dictionary

--- homepage/forms.py
+++ homepage/forms.py
@@ -8,7 +8,7 @@ class SearchForm(forms.ModelForm):
     value = forms.CharField(strip=False, widget=forms.PasswordInput(attrs={
         "class": "form-control form-control-dark me-1",
         "type": "text",
-        "placeholder": "Ключивое слово/фраза",
+        "placeholder": "Ключевое слово/фраза",
         "id": "value",
     }), )
 
